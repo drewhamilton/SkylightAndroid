2cda8271baa6238d06f57fb86a63490924a6c99a
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-06-30 17:09:25 +0200

    Use factory in :nightmode

--- nightmode/src/main/java/drewhamilton/skylight/android/nightmode/AutoNightActivity.kt
+++ nightmode/src/main/java/drewhamilton/skylight/android/nightmode/AutoNightActivity.kt
@@ -2,6 +2,7 @@ package drewhamilton.skylight.android.nightmode
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import drewhamilton.skylight.android.factory.AndroidSkylightFactory
 
 /**
  * A base Activity that sets the current night mode using an [AutoNightDelegate].
@@ -14,7 +15,9 @@ abstract class AutoNightActivity : AppCompatActivity() {
      * Uses [AutoNightDelegate.fallback] by default. Should be overridden if a different [AutoNightDelegate] is
      * required.
      */
-    protected open val autoNightDelegate: AutoNightDelegate by lazy { AutoNightDelegate.fallback(delegate) }
+    protected open val autoNightDelegate: AutoNightDelegate by lazy {
+        AutoNightDelegate(delegate, AndroidSkylightFactory.createForLocation(this))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
