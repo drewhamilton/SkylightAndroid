e50d72b6ca68b880f38a75da16d3b0457a924ac2
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-10-10 20:21:15 +0200

    Ensure saved dark mode is applied at application launch

--- app/src/main/java/drewhamilton/skylight/android/sample/AppComponent.kt
+++ app/src/main/java/drewhamilton/skylight/android/sample/AppComponent.kt
@@ -5,6 +5,7 @@ import dagger.BindsInstance
 import dagger.Component
 import drewhamilton.skylight.android.sample.main.MainActivity
 import drewhamilton.skylight.android.sample.settings.SettingsActivity
+import drewhamilton.skylight.android.sample.theme.MutableThemeRepository
 import drewhamilton.skylight.backport.dummy.dagger.DummySkylightComponent
 import drewhamilton.skylight.backport.sso.dagger.SsoSkylightComponent
 import javax.inject.Singleton
@@ -16,7 +17,10 @@ import javax.inject.Singleton
 ])
 interface AppComponent {
 
+    fun themeRepository(): MutableThemeRepository
+
     fun inject(mainActivity: MainActivity)
+
     fun inject(settingsActivity: SettingsActivity)
 
     @Component.Factory interface Factory {
